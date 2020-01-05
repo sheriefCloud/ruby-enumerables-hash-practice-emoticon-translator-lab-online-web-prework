9a8578f704de27f09f3059627caaf214544fe551
Sherief Farid <sherief@farid.cloud>
2020-01-05 02:38:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,23 @@
 # require modules here
+require "yaml"
+require "pry"
 
-def load_library
-  # code goes here
+def load_library(sourcefile)
+  loadedfile = YAML.load_file sourcefile
+  # Key = meaning | value = array | arary[0] = english && array[1] = Japanees
+
+  emos = {}
+
+  emos[:get_meaning] = {}
+  emos[:get_emoticon] = {}
+
+  loadedfile.each() { |key, value| emos[:get_meaning][value[1]] = key }
+  loadedfile.each() do |key, value|
+    e_emo_value = loadedfile[key][value[0]]
+    emos[:get_emoticon][value[0]] = e_emo_value
+
+  end
+  emos
 end
 
 def get_japanese_emoticon
