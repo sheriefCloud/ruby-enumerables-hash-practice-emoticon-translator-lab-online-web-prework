d1e48e164badc62b1b7172cdbbe14d9ba2c11a3d
Sherief Farid <sherief@farid.cloud>
2020-01-05 03:54:53 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,32 +5,20 @@ require "pry"
 def load_library(sourcefile)
   loadedfile = YAML.load_file sourcefile
   # Key = meaning | value = array | arary[0] = english && array[1] = Japanees
-
   emos = {}
-
   emos[:get_meaning] = {}
   emos[:get_emoticon] = {}
-
   loadedfile.each() { |key, value| emos[:get_meaning][value[1]] = key }
   loadedfile.each() { |key, value| emos[:get_emoticon][value[0]] = value[1] }
-
   emos
 end
 
 def get_japanese_emoticon(file,emoticon)
-
   data_loaded = load_library(file)
-  message = ""
-  return_value = data_loaded[:get_emoticon].fetch(emoticon, "Sorry, that emoticon was not found")# {|key| key == emoticon }
-#binding.pry
-  # if return_value == nil
-  #   message = "I am sory"
-  # else
-  #   message = return_value
-  # end
-
+  return_value = data_loaded[:get_emoticon].fetch(emoticon, "Sorry, that emoticon was not found")
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file,emoticon)
+  data_loaded = load_library(file)
+  return_value = data_loaded[:get_meaning].fetch(emoticon, "Sorry, that emoticon was not found")
 end
